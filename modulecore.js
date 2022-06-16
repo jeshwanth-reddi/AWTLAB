cc74b30259d58b9563bb1ed159eeaaa0982950ca
jeshwanth-reddi <reddyjeshwanth@gmail.com>
2022-06-16 23:18:11 +0530

    awtlab

--- modulecore.js
+++
@@ -1,7 +0,0 @@
-var Http=require('http')
-var server=Http.createServer(function(req,res){
-    res.write("Hello world");
-    res.end();
-})
-server.listen(2000)
-console.log("Server started in port 2000");
